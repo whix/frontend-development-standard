a96cff51d9ab74942ea58292d8d75d9d11b3db97
huanghewan <huanghewan@cloudbility.com>
2026-03-23 16:23:23 +0800

    feat: add live preview surfaces

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -2,3 +2,33 @@ import { TextDecoder, TextEncoder } from "node:util";
 
 globalThis.TextEncoder = TextEncoder;
 globalThis.TextDecoder = TextDecoder;
+
+class MockResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+Object.defineProperty(globalThis, "ResizeObserver", {
+  configurable: true,
+  writable: true,
+  value: MockResizeObserver
+});
+
+Object.defineProperty(globalThis, "matchMedia", {
+  configurable: true,
+  writable: true,
+  value: (query: string) =>
+    ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener() {},
+      removeListener() {},
+      addEventListener() {},
+      removeEventListener() {},
+      dispatchEvent() {
+        return false;
+      }
+    }) as MediaQueryList
+});
